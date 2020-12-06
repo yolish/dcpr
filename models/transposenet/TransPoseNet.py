79bf0d06022cbe0a0a6222fd060997b678393852
yolish <yolisha@gmail.com>
2020-12-06 21:46:58 +0200

    Update TransPoseNet.py
    Changed to dictionary-based API for extra flexibility

--- models/transposenet/TransPoseNet.py
+++ models/transposenet/TransPoseNet.py
@@ -13,42 +13,49 @@ from .backbone import build_backbone
 class TransPoseNet(nn.Module):
 
     def __init__(self, config, pretrained_path):
-        """ Initializes the model.
+        """
+        config: (dict) configuration of the model
+        pretrained_path: (str) path to the pretrained backbone
         """
         super().__init__()
 
         config["backbone"] = pretrained_path
         config["learn_embedding_with_pose_token"] = True
+
+        # CNN backbone
         self.backbone = build_backbone(config)
+
+        # Position (t) and orientation (rot) encoders
         self.transformer_t = Transformer(config)
         self.transformer_rot = Transformer(config)
 
         decoder_dim = self.transformer_t.d_model
+
+        # The learned pose token for position (t) and orientation (rot)
         self.pose_token_embed_t = nn.Parameter(torch.zeros((1, decoder_dim)), requires_grad=True)
         self.pose_token_embed_rot = nn.Parameter(torch.zeros((1, decoder_dim)), requires_grad=True)
+
+        # The projection of the activation map before going into the Transformer's encoder
         self.input_proj_t = nn.Conv2d(self.backbone.num_channels[0], decoder_dim, kernel_size=1)
         self.input_proj_rot = nn.Conv2d(self.backbone.num_channels[1], decoder_dim, kernel_size=1)
+
+        # Whether to use prior from the position for the orientation
         self.use_prior = config.get("use_prior_t_for_rot")
+
+        # Regressors for position (t) and orientation (rot)
         self.regressor_head_t = PoseRegressor(decoder_dim, 3)
         self.regressor_head_rot = PoseRegressor(decoder_dim, 4, self.use_prior)
 
-    def forward_encoder(self, samples: NestedTensor):
+    def forward_transformers(self, data):
         """
-        The forward expects a NestedTensor, which consists of:
+        The forward pass expects a dictionary with key-value 'img' -- NestedTensor, which consists of:
                - samples.tensor: batched images, of shape [batch_size x 3 x H x W]
                - samples.mask: a binary mask of shape [batch_size x H x W], containing 1 on padded pixels NOT USED
+        return a dictionary with the following keys--values:
+            global_desc_t: latent representation from the position encoder
+            global_dec_rot: latent representation from the orientation encoder
         """
-        multiple_crops = samples.shape[1] > 3
-        batch_size = samples.shape[0]
-
-        # Handle multiple crops if needed (flatten)
-        num_crops_per_img = 1
-
-        if multiple_crops:
-            num_crops_per_img = int(samples.shape[1] / 3)
-            samples = samples.contiguous().view(batch_size, num_crops_per_img, 3, samples.shape[2], samples.shape[3])
-            samples = samples.contiguous().view(batch_size * num_crops_per_img, 3, samples.shape[3],
-                                                samples.shape[4])  # N*NUM_CROPS X C X H X W
+        samples = data.get('img')
 
         # Handle data structures
         if isinstance(samples, (list, torch.Tensor)):
@@ -71,43 +78,48 @@ class TransPoseNet(nn.Module):
         global_desc_t = local_descs_t[:, 0, :]
         global_desc_rot = local_descs_rot[:, 0, :]
 
-        # Handle multiple crops
-        if num_crops_per_img > 1:
-            j = 0
-            for i in range(batch_size):
-                global_desc_t[i] = global_desc_t[j:(j + num_crops_per_img), :].mean(dim=0)
-                global_desc_rot[i] = global_desc_rot[j:(j + num_crops_per_img), :].mean(dim=0)
-                j = j + num_crops_per_img
-            global_desc_t = global_desc_t[:batch_size, :]
-            global_desc_rot = global_desc_rot[:batch_size, :]
+        return {'global_desc_t':global_desc_t, 'global_desc_rot':global_desc_rot}
 
-        return global_desc_t, global_desc_rot
+    def forward_heads(self, transformers_res):
+        """
+        The forward pass execpts a dictionary with two keys-values:
+        global_desc_t: latent representation from the position encoder
+        global_dec_rot: latent representation from the orientation encoder
+        returns: dictionary with key-value 'pose'--expected pose (NX7)
+        """
+        global_desc_t = transformers_res.get('global_desc_t')
+        global_desc_rot = transformers_res.get('global_desc_rot')
 
-    def forward_heads(self, global_desc_t, global_desc_rot):
         x_t = self.regressor_head_t(global_desc_t)
         if self.use_prior:
             global_desc_rot = torch.cat((global_desc_t, global_desc_rot), dim=1)
+
         x_rot = self.regressor_head_rot(global_desc_rot)
         expected_pose = torch.cat((x_t, x_rot), dim=1)
-        return expected_pose
+        return {'pose': expected_pose}
 
-    def forward(self, samples: NestedTensor):
-        """ The forward expects a NestedTensor, which consists of:
+    def forward(self, data):
+        """ The forward pass expects a dictionary with key-value 'img' -- NestedTensor, which consists of:
                - samples.tensor: batched images, of shape [batch_size x 3 x H x W]
                - samples.mask: a binary mask of shape [batch_size x H x W], containing 1 on padded pixels NOT USED
 
-            returns an expected pose
+            returns dictionary with key-value 'pose'--expected pose (NX7)
         """
-        global_desc_t, global_desc_rot = self.forward_encoder(samples)
+        transformers_encoders_res = self.forward_transformers(data)
         # Regress the pose from the image descriptors
-        expected_pose = self.forward_heads(global_desc_t, global_desc_rot)
-        return expected_pose
+        heads_res = self.forward_heads(transformers_encoders_res)
+        return heads_res
 
 
 class PoseRegressor(nn.Module):
-    """ A simple pose regressor"""
+    """ A simple MLP to regress a pose component"""
 
     def __init__(self, decoder_dim, output_dim, use_prior=False):
+        """
+        decoder_dim: (int) the input dimension
+        output_dim: (int) the outpur dimension
+        use_prior: (bool) whether to use prior information
+        """
         super().__init__()
         ch = 1024
         self.fc_h = nn.Linear(decoder_dim, ch)
@@ -123,6 +135,9 @@ class PoseRegressor(nn.Module):
                 nn.init.xavier_uniform_(p)
 
     def forward(self, x):
+        """
+        Forward pass
+        """
         if self.use_prior:
             x = F.gelu(self.fc_h_prior(x))
         else:
