58200a283a13080a9cc9cd25b763b8db99475ffa
yolish <yolisha@gmail.com>
2021-02-16 08:59:18 +0200

    Update PoseNet.py
    Updated the posenet implementation

--- models/posenet/PoseNet.py
+++ models/posenet/PoseNet.py
@@ -1,13 +1,11 @@
 import torch
 import torch.nn as nn
-import torchvision
 import torch.nn.functional as F
-import copy
 
 
 class PoseNet(nn.Module):
     """
-    A class to represent a classic pose regressor (PoseNet) with resnet-152 backbone
+    A class to represent a classic pose regressor (PoseNet) with an efficient-net backbone
     PoseNet: A Convolutional Network for Real-Time 6-DOF Camera Relocalization,
     Kendall et al., 2015
     """
@@ -18,19 +16,17 @@ class PoseNet(nn.Module):
         """
         super(PoseNet, self).__init__()
 
-        # Load resnet 152
-        backbone = torchvision.models.resnet152(pretrained=False)
-        backbone.load_state_dict(torch.load(backbone_path))
-
-        # Remove the classifier heads and pooling
-        self.backbone = copy_modules(backbone, 0, -2)
+        # Efficient net
+        self.backbone = torch.load(backbone_path)
+        backbone_dim = 1280
+        latent_dim = 1024
 
         # Regressor layers
-        self.fc1 = nn.Linear(2048, 1024)
-        self.fc2 = nn.Linear(1024, 3)
-        self.fc3 = nn.Linear(1024, 4)
+        self.fc1 = nn.Linear(backbone_dim, latent_dim)
+        self.fc2 = nn.Linear(latent_dim, 3)
+        self.fc3 = nn.Linear(latent_dim, 4)
 
-        self.dropout = nn.Dropout(p=0.2)
+        self.dropout = nn.Dropout(p=0.1)
         self.avg_pooling_2d = nn.AdaptiveAvgPool2d(1)
 
         # Initialize FC layers
@@ -44,37 +40,11 @@ class PoseNet(nn.Module):
         :param data: (torch.Tensor) dictionary with key-value 'img' -- input image (N X C X H X W)
         :return: (torch.Tensor) dictionary with key-value 'pose' -- 7-dimensional absolute pose for (N X 7)
         """
-        x = data.get('img')
-        x = self.avg_pooling_2d(self.backnone(x))  # N X 3 X 224 X 224 -> Nx2048x7x7 -> Nx2048x1
-        x = x.view(x.size(0), -1)  # output shape Nx2048
-
-        x = self.dropout(F.relu(self.fc1_gp(x)))
+        x = self.backbone.extract_features(data.get('img'))
+        x = self.avg_pooling_2d(x)
+        x = x.flatten(start_dim=1)
+        x = self.dropout(F.relu(self.fc1(x)))
         p_x = self.fc2(x)
         p_q = self.fc3(x)
         return {'pose': torch.cat((p_x, p_q), dim=1)}
-
-
-def copy_modules(model, start_idx, end_idx):
-    """
-    Copy modules from a model
-    :param net: (nn.Module) the network to copy
-    :param start_idx: (int) index of the module where the copy should start
-    :param end_idx: (int) index of the module where the copy should end (exclusive)
-    :return: deep copy of submodel
-    """
-    modules = list(model.children())[start_idx:end_idx]
-
-    # Copy the modules
-    sub_model = nn.Sequential(*modules)
-    params_orig = model.state_dict()
-    params_truncated = sub_model.state_dict()
-
-    # Copy the parameters
-    for name, param in params_orig.items():
-        if name in params_truncated:
-            params_truncated[name].data.copy_(param.data)
-
-    # Load parameters into the architecture
-    sub_model.load_state_dict(params_truncated)
-    return copy.deepcopy(sub_model)
 
